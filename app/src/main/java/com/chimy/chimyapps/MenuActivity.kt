90db8899904881437adf1498235bdd3f8ee3c497
michimurri <joselo.okumura@gmail.com>
2023-05-25 18:28:38 -0300

    agregando la app imc al conjunto

--- app/src/main/java/com/chimy/chimyapps/MenuActivity.kt
+++ app/src/main/java/com/chimy/chimyapps/MenuActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.chimy.chimyapps.IMCcalculator.ImcCalculatorActivity
 import com.chimy.chimyapps.firstApp.FirstAppActivity
 
 
@@ -12,10 +13,17 @@ class MenuActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
         val btnSaludapp = findViewById<Button>(R.id.btnSaludapp)
+        val btnImcCalculatorActivity = findViewById<Button>(R.id.btnIMCApp)
+        btnImcCalculatorActivity.setOnClickListener{navigateToIMCApp()}
         btnSaludapp.setOnClickListener{navigateToSaludapp()}
     }
 
-    fun navigateToSaludapp(){
+    private fun navigateToIMCApp() {
+        val intent = Intent(this, ImcCalculatorActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToSaludapp(){
         val intent = Intent(this, FirstAppActivity::class.java)
         startActivity(intent)
     }
